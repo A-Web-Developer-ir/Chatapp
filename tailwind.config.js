82f72438f601d5454502976f6eecb3e1478a1b85
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-07-26 13:27:22 +0430

    add public folder to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.{html,js,ejs}"],
+  content: ["./views/**/*.{html,js,ejs}", "./public/**/*.js"],
   theme: {
     extend: {},
   },
