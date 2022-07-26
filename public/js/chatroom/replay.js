b8f12d395760206865476fda184eeba3639f17d4
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-07-26 13:28:19 +0430

    add replay and unReplay functions

--- public/js/chatroom/replay.js
+++ public/js/chatroom/replay.js
@@ -1,5 +1,15 @@
-console.log("hllllll");
+export let replayId = null;
 
-export function myFunction() {
+const replayBox = document.getElementById("replayBox");
 
+window.replay = (messageElement) => {
+    replayId = messageElement.dataset.messageid;
+    replayBox.querySelector("p").innerHTML = messageElement.querySelector("p").innerHTML;
+    replayBox.classList.remove("hidden");
+    replayBox.classList.add("flex");
+}
+window.unReplay = () => {
+    replayId = null;
+    replayBox.classList.add("hidden");
+    replayBox.classList.remove("flex");
 }
