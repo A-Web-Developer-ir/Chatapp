81c4608668c21129f6b4487e49e6cb09dce89d42
A-Web-Developer-ir <a.web.developer.ir@gmail.com>
2022-07-27 23:04:59 +0430

    a few change in infoMassege.js

--- routes/infoMessage.js
+++ routes/infoMessage.js
@@ -7,6 +7,7 @@ const messageSchema = require('../schemas/messageSchema');
 app.get("/info/message", cors({ origin: "*" }), async (req, res) => {
     const id = req.query.id;
     const result = await messageSchema.findById(id);
+    if (!result) return res.status(404).json({ message: "not found" })
     res.json({
         message: result.message,
         name: result.name
