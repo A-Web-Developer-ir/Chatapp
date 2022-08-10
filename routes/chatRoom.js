d2c43097300049ee1370ca6898ccb84d25db9396
SamuelSam-dev <xsamansafaeix@gmail.com>
2022-08-11 00:17:36 +0430

    upgrade site

--- routes/chatRoom.js
+++ routes/chatRoom.js
@@ -2,12 +2,12 @@ const { Router } = require("express");
 const messageSchema = require("../schemas/messageSchema");
 const app = Router();
 
-  app.get("/chatroom", async(req, res) => {
-    if (!req.session.loggedIn) {
-      res.redirect("/singin");
-    } else {
-      const result = await messageSchema.find({});
-      res.render("page/chatroom_public", { data: result });
-    }
-  });
-  module.exports = app;
+app.get("/chatroom", async (req, res) => {
+  if (!req.session.loggedIn) {
+    res.redirect("/signin");
+  } else {
+    const result = await messageSchema.find({});
+    res.render("page/chatroom_public", { data: result });
+  }
+});
+module.exports = app;
